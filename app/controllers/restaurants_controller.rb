dd713942407e1903f6bb3fd2144b1565c269206e
Benoit Prigent <benoit.prigent@grenoble-em.com>
2018-02-14 15:54:59 +0100

    implemented new routes

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,22 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @new_restaurant = params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+    @restaurant = Restaurant.new(@new_restaurant)
+    @restaurant.save!
+    redirect_to restaurants_path
+  end
+
+end
