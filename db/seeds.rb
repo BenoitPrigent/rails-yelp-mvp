f8107e81bf4e88df536f3859e26af89e5f173a1e
Benoit Prigent <benoit.prigent@grenoble-em.com>
2018-02-14 14:36:15 +0100

    seeding the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '06 04 18 57 55',
+    category:       "chinese"
+  },
+  {
+    name:         'Asian Wok',
+    address:      'Loin',
+    phone_number:  '06 04 18 57 55',
+    category:       "french"
+  },
+  {
+    name:         'Esprit Tchai',
+    address:      'Près',
+    phone_number:  '06 04 28 57 55',
+    category:       "italian"
+  },
+  {
+    name:         'Phorrito',
+    address:      'Tout prêt',
+    phone_number:  '06 12  22 57 55',
+    category:       "belgian"
+  },
+  {
+    name:         'Glasses',
+    address:      'New',
+    phone_number:  '06 04 18 22 55',
+    category:       "japanese"
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
